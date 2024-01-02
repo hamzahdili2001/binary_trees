c28a8408a41add8d8ee877b35ac1af4c79d1352e
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-02 14:05:35 +0100

    [add] function that inserts a node as the right-child of another node

--- binary_trees.h
+++ binary_trees.h
@@ -22,4 +22,5 @@ typedef struct binary_tree_s binary_tree_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 #endif /* _BINART_TREE_H_ */
