bf22fa1f1c2fe375ad3d9f99f227b27c11b68b2e
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-03 19:10:55 +0100

    [add] function that performs a right-rotation on a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -42,4 +42,5 @@ int binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 #endif /* _BINART_TREE_H_ */
