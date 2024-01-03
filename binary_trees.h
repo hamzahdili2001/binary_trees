d59d274ac03a6bcc8f94b9616c8a0a7d7d23e150
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-03 18:24:58 +0100

    [add]  function that finds the lowest common ancestor of two nodes

--- binary_trees.h
+++ binary_trees.h
@@ -40,4 +40,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 #endif /* _BINART_TREE_H_ */
