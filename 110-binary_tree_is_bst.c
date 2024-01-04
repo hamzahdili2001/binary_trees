cb49d1c67350b9279a1cf550cf577ae91c92545c
MrSakine <msakine20@gmail.com>
2024-01-04 19:00:52 +0000

    [task-110] logic update

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -11,9 +11,7 @@
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	binary_tree_t *temp = NULL;
-
-	return (binary_tree_is_bst_util(tree, temp));
+	return (binary_tree_is_bst_util(tree, NULL, NULL));
 }
 
 /**
@@ -21,25 +19,26 @@ int binary_tree_is_bst(const binary_tree_t *tree)
  * is a valid Binary Search Tree
  *
  * @tree: pointer to the root node of the tree to check
- * @node: pointer to temporary node
+ * @min: min node
+ * @max: max node
  *
  * Return:
  * - 1 if tree is a valid BST, and 0 otherwise
  */
-int binary_tree_is_bst_util(const binary_tree_t *tree, binary_tree_t *node)
+int binary_tree_is_bst_util(
+	const binary_tree_t *tree,
+	const binary_tree_t *min,
+	const binary_tree_t *max)
 {
-	if (tree)
-	{
-		if (!binary_tree_is_bst_util(tree->left, node))
-			return (0);
+	if (tree == NULL)
+		return (1);
 
-		if (node != NULL && tree->n <= node->n)
-			return (0);
+	if (
+		(min != NULL && tree->n <= min->n) ||
+		(max != NULL && tree->n >= max->n))
+		return (0);
 
-		node = (binary_tree_t *)tree;
-
-		return (binary_tree_is_bst_util(tree->right, node));
-	}
-
-	return (1);
+	return (
+		binary_tree_is_bst_util(tree->left, min, tree) &&
+		binary_tree_is_bst_util(tree->right, tree, max));
 }
