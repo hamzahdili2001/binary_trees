c18720949d8a8868e0c3d46716d4e5e2b1582da5
hamzahdili2001 <hdilihamza955@gmail.com>
2024-01-04 19:40:14 +0100

    [fix] task-112: change the functions add one function that build a binary search tree from an array

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,20 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * free_tree - Frees a BST and its nodes.
- * @tree: A pointer to the root node of the BST.
- */
-void free_tree(bst_t *tree)
-{
-	if (tree != NULL)
-	{
-		free_tree(tree->left);
-		free_tree(tree->right);
-		free(tree);
-	}
-}
-
-/**
  * array_to_bst - Builds a Binary Search Tree from an array.
  * @array: A pointer to the first element of the array to be converted.
  * @size: The number of elements in the array.
@@ -24,18 +10,24 @@ void free_tree(bst_t *tree)
 bst_t *array_to_bst(int *array, size_t size)
 {
 	bst_t *root = NULL;
-	size_t i;
+	size_t i, j;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
-		if (bst_insert(&root, array[i]) == NULL)
+		for (j = 0; j < i; j++)
 		{
-			free_tree(root);
-			return (NULL);
+			if (array[j] == array[i])
+				break;
+		}
+		if (j == i)
+		{
+			if (bst_insert(&root, array[i]) == NULL)
+				return (NULL);
 		}
 	}
+
 	return (root);
 }
